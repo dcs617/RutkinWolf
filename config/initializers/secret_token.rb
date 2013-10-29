5dff5de25ef14b90684b27f4a9888eff00b9aae2
Daniel Sparano <dsparano1@gmail.com>
2013-10-29 15:09:02 -0400

    Initial Mac Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RutkinWolf::Application.config.secret_token = '5250cd04a60ffc47bc0012cf15befb8d5d75750ccd79ef7af54bbc923ecfb5909939f4fecf40ce1f62d54d80740e1701cded9c4f0cf3482aa35d6f8c80075b0d'
